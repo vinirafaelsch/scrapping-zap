7ec0161052440de5b472bb0a82a317e262237a4b
vinirafaelsch <vinirafaelsch@gmail.com>
2024-07-25 04:34:49 -0300

    mudança e formatação em alguns campos

--- handle_file.py
+++ handle_file.py
@@ -1,4 +1,5 @@
 import json
+import locale
 
 from datetime import datetime
 
@@ -6,6 +7,8 @@ class HandleFile:
     def __init__(self, data):
         self.extracted_data: list = data
         self.geojson_data: dict = {}
+
+        locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
     def data2geojson(self):
         geojson = {
@@ -16,25 +19,26 @@ class HandleFile:
                 {
                     "type": "Feature",
                     "properties": {
-                        "id": data.get('id', ""),
-                        "cidade": data.get('address', {}).get('city', ""),
-                        "rua": data.get('address', {}).get('street', ""),
-                        "bairro": data.get('address', {}).get('neighborhood', ""),
-                        "numero": data.get('address', {}).get('streetNumber', ""),
+                        "id": self.format_value(data.get('id', "")),
+                        "cidade": self.format_value(data.get('address', {}).get('city', "")),
+                        "rua": self.format_value(data.get('address', {}).get('street', "")),
+                        "bairro": self.format_value(data.get('address', {}).get('neighborhood', "")),
+                        "numero": self.format_value(data.get('address', {}).get('streetNumber', "")),
                         # "lat": data.get('address', {}).get('coordinate', {}).get('lat', ""),
                         # "lng": data.get('address', {}).get('coordinate', {}).get('lng', ""),
                         # "radius": data.get('address', {}).get('coordinate', {}).get('radius', ""),
-                        "valor": data.get('prices', {}).get('mainValue', ""),
-                        "iptu": data.get('prices', {}).get('iptu', ""),
-                        "condominium": data.get('prices', {}).get('condominium', ""),
-                        "descricao": data.get('description', ""),
-                        "area": data.get('amenities', {}).get('usableAreas', ""),
-                        "quartos": data.get('amenities', {}).get('bedrooms', ""),
-                        "banheiros": data.get('amenities', {}).get('bathrooms', ""),
-                        "estacionamentos": data.get('amenities', {}).get('parkingSpaces', ""),
-                        "url": data.get('url', ""),
-                        "tipo_negocio": data.get('business', ""),
-                        "tipo_unidade": data.get('unitTypes', "")
+                        "valor": self.value2real(self.format_value(data.get('prices', {}).get('mainValue', ""))),
+                        "iptu": self.value2real(self.format_value(data.get('prices', {}).get('iptu', ""))),
+                        "condominium": self.value2real(self.format_value(data.get('prices', {}).get('condominium', ""))),
+                        "descricao": self.format_value(data.get('description', "")),
+                        "area": self.format_value(data.get('amenities', {}).get('usableAreas', "")),
+                        "quartos": self.format_value(data.get('amenities', {}).get('bedrooms', "")),
+                        "banheiros": self.format_value(data.get('amenities', {}).get('bathrooms', "")),
+                        "estacionamentos": self.format_value(data.get('amenities', {}).get('parkingSpaces', "")),
+                        "url": self.format_value(data.get('url', "")),
+                        "tipo_negocio": self.format_value(data.get('business', "")),
+                        "tipo_imovel": self.handle_unit_types(data.get('unitTypes', "")),
+                        "data_extracao": datetime.today().strftime('%d-%m-%Y')
                     },
                     "geometry": {
                         "type": "Point",
@@ -48,6 +52,28 @@ class HandleFile:
         }
 
         return geojson
+    
+    def format_value(self, value):
+        if isinstance(value, type(None)) or value == "":
+            return "Não informado"
+        return value
+
+    def value2real(self, value):
+        if value != "Não informado":
+            return locale.currency(float(value), grouping=True)
+        return value
+    
+    def handle_unit_types(self, data):
+        if data == "Não informado":
+            return "Não informado"
+        
+        if len(data) == 1:
+            return data[0]
+        
+        string = ""
+        for value in data:
+            string = (f'{string}; {value}') if not string == "" else value
+        return string
 
     def create_file(self):
         self.geojson_data = self.data2geojson()
